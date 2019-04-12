51ea07d9de3ba4d933cf7df08f8fd48a639de038
YJT_glb <981021648_glb@qq.com>
2019-04-12 15:37:31 +0800

    插入排序

---
+++ InsertionSort/src/Code_01_InsertionSort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Code_01_InsertionSort {
+    public static void insertionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+    public static void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={4,8,9,4,5,3,2,7};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
