27eede9cde08b7eacd8db53b83e81a2cb3470c44
YJT_glb <981021648_glb@qq.com>
2019-04-15 18:46:01 +0800

    判断一个链表是否为回文结构

---
+++ IsPalindromeList/src/Code_11_IsPalindromeList.java
@@ -0,0 +1,52 @@
+
+import java.util.Stack;
+public class Code_11_IsPalindromeList {
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+    // need n extra space
+    public static boolean isPalindrome1(Node head) {
+        Stack<Node> stack = new Stack<Node>();
+        Node cur = head;
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.next;
+        }
+        while (head != null) {
+            if (head.value != stack.pop().value) {
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+    // need n/2 extra space
+    public static boolean isPalindrome2(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Node right = head.next;
+        Node cur = head;
+        while (cur.next != null && cur.next.next != null) {
+            right = right.next;
+            cur = cur.next.next;
+        }
+        Stack<Node> stack = new Stack<Node>();
+        while (right != null) {
+            stack.push(right);
+            right = right.next;
+        }
+        while (!stack.isEmpty()) {
+            if (head.value != stack.pop().value) {
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+}
