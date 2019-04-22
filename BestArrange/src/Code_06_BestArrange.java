5bf11e03c62472c8d9132adba82da54b882b58b1
YJT_glb <981021648_glb@qq.com>
2019-04-22 15:27:05 +0800

    宣讲会场次安排

---
+++ BestArrange/src/Code_06_BestArrange.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Code_06_BestArrange {
+    public static class Program{
+        public int start;
+        public int end;
+
+        public Program(int start,int end){
+            this.start=start;
+            this.end=end;
+        }
+    }
+    public static class ProgramComparator implements Comparator<Program>{
+        @Override
+        public int compare(Program o1, Program o2) {
+            return o1.end - o2.end;
+        }
+    }
+//cur当前时刻
+    public static int bestArrange(Program[] programs,int cur){
+        Arrays.sort(programs,new ProgramComparator());
+        int result=0;
+        for(int i=0;i<programs.length;i++){
+            if(cur<=programs[i].start) {
+                result++;
+                cur = programs[i].end;
+            }
+        }
+        return result;
+    }
+
+
+}
